0c7e119e0b35f64af87092d3e293b677556f79df
MrViper111 <74232706+MrViper111@users.noreply.github.com>
2023-01-11 21:20:50 -0800

    Initial commit

---
+++ Example.java
@@ -0,0 +1,14 @@
+public class Example {
+
+    public static void main(String[] args) {
+        boolean knowsGit = false;
+
+        if (knowsGit) {
+            System.out.println("I know Git!");
+        } else {
+            System.out.println("I am a loser and don't know Git.");
+        }
+
+    }
+
+}
